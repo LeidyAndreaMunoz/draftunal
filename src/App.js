0bb09c697c48772dda7a22ef46949184f6441234
Leidy Muñoz <leidy.munoz@alteru.co>
2021-08-28 14:02:09 -0500

    First commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './assets/styles.css';
+import { Pages } from './Pages/pages';
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <Pages/>
+    </div>
+  );
+}
+
+export default App;
