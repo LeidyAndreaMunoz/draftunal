24e299cd58d6d53070824f6c53b3dfeb7cdb55d4
Leidy Muñoz <leidy.munoz@alteru.co>
2021-08-30 06:21:10 -0500

    avance

--- src/Components/Texts/TextFieldCode/TextfieldCode.jsx
+++ src/Components/Texts/TextFieldCode/TextfieldCode.jsx
@@ -3,8 +3,6 @@ import './TextfieldCode.css'
 import TextField from '@material-ui/core/TextField';
 import { withStyles } from '@material-ui/core';
 
-const DescriptionT = "Gastos destinados a..... Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla eleifend lorem ut sapien pulvinar auctor non vulputate diam. Nam consequat dolor ac finibus vestibulum. Fusce rutrum pellentesque lorem, quis sodales nisl. Ut vel dolor a mauris maximus pellentesque. Nam congue congue lectus id dictum. Aenean rhoncus a est at scelerisque."
-const NameT = "Accesorios para encuadernadores de hojas intercambiables y para clasificadores; sujetadores, cantoneras, clips, índices señaladores y artículos análogos para oficina, de metales comunes; grapas en tiras, de metales comunes"
 
 const CssTextField = withStyles({
     root: {
--- src/Pages/pages.jsx
+++ src/Pages/pages.jsx
@@ -1,8 +1,48 @@
 import React from 'react';
-import { Typography, Button, Dialog, DialogActions, DialogContent, DialogTitle, Divider, Grid,Paper } from '@material-ui/core';
-import { EditModal } from '../Components/Modals/EditModal/EditModal';
+import './pages.css';
+import {  Dialog, DialogActions, DialogContent, DialogTitle, Divider, Grid,Paper, withStyles, } from '@material-ui/core';
 import { TextfielDescription } from '../Components/Texts/TextFieldCode/TextfieldCode'
 import { DefinitionText } from '../Components/Texts/Definition/DefinitionText';
+import { AcceptButton } from '../Components/Buttons/AcceptButton/Accept';
+import { CancelButton } from '../Components/Buttons/CancelButton/Cancel';
+import { SaveIconB } from '../Components/Icons/Icons';
+
+const ActionsDialog = withStyles({
+  root: {
+      display: 'flex',
+      padding: 8,
+      alignItems: 'center',
+      justifyContent: 'space-around',
+      },    
+  },
+)(DialogActions);
+
+const EditDialog = withStyles({
+  root: {
+      '& .MuiDialog-paperFullWidth': {
+        width: "100%",
+        padding: "1%",
+        paddingTop: 0,
+        borderRadius: 16,
+      },
+      '& .MuiOutlinedInput-multiline': {
+        padding: '1%',
+        margin: '2%',
+      }, '& .MuiGrid-spacing-xs-5 .MuiGrid-item': {
+        padding: '7%'
+      }
+      },    
+  },
+)(Dialog); 
+const GridField = withStyles({
+  root: {
+      '& .MuiGrid-item': {
+          display: 'grid',
+          width: "80%",
+      },
+      },    
+  },
+)(Grid);
 
 export const Pages =() => {
     const [ info, setInfo ] = React.useState({title:"", description:""});
@@ -15,7 +55,15 @@ export const Pages =() => {
             title: modal.title,
             description: modal.description,
             keys: modal.keys,
-            button: modal.button
+            buttonAccept: modal.buttonAccept,
+            buttonOpen: modal.buttonOpen,
+            buttonOpenText: modal.buttonOpenText, 
+            buttonClose: modal.buttonClose,
+            buttonCloseText: modal.buttonCloseText,
+            success: modal.success,
+            error: modal.error,
+            desciptionAlert: modal.desciptionAlert,
+            saveIcon: modal.saveIcon,
         })
         setOpen(true);
     }
@@ -23,22 +71,22 @@ export const Pages =() => {
       setOpen(false);
     };
 
-    const cerrrarModal = () => {
-      setOpen(false)
-    }
     const cerrrarModalAbrirAlerta = () => {
-      setOpen(false)
+      setOpen(true)
       setopenAlert(true)
     }
 
     const handleCloseAlert = () => {
       setopenAlert(false)
+      setOpen(false)
     }
-    
     const valuesPerModals = {
         addSection: {
           title: 'Agregar Seccion',
-          description: 'xd1',
+          description: 'Por favor diligencia la siguiente información completando los campos obligatorios(*).',
+          saveIcon: <SaveIconB/>,
+          buttonOpenText: 'GUARDAR',
+          buttonCloseText: 'CANCELAR',
           keys: [
             {
                 name: 'codeNewSecction',
@@ -53,11 +101,14 @@ export const Pages =() => {
                 label: 'Definicion de la Nueva Seccion'
             },
           ],
-          button: () => cerrrarModal()
+          buttonOpen: () => cerrrarModalAbrirAlerta(),
+          buttonClose: () => handleClose(),  
+          success: 'Sección creada exitosamente'
         },
         editSection: {
           title: 'Editar Seccion',
-          description: 'xd2',
+          buttonOpenText: 'GUARDAR',
+          buttonCloseText: 'CANCELAR',
           keys: [
             {
                 name: 'codeSecction',
@@ -72,136 +123,180 @@ export const Pages =() => {
                 label: 'Definicion de la Seccion'
             },
           ],
-          button: () => cerrrarModalAbrirAlerta()
+          buttonOpen: () => cerrrarModalAbrirAlerta(),
+          buttonClose: () => handleClose(),
+          success: 'Sección editada exitosamente'      
         },
         editDivision: {
           title: 'Editar Division',
-          description: 'xd3',
+          buttonOpenText: 'GUARDAR',
+          buttonCloseText: 'CANCELAR',
           keys: [
             {
-                name: '',
-                label: ''
+                name: 'codeDivision',
+                label: 'Codigo de la División'
             },
             {
-                name: '',
-                label: ''
+                name: 'nameDivision',
+                label: 'Nombre de la División'
             },
             {
-                name: '',
-                label: ''
+                name: 'descriptionDivision',
+                label: 'Definicion de la División'
             },
-          ]
+          ],
+          buttonOpen: () => cerrrarModalAbrirAlerta(),
+          buttonClose: () => handleClose(),   
+          success: 'División editada exitosamente'   
         },
         editGroup: {
           title: 'Editar Grupo',
-          description: 'xd4',
+          buttonOpenText: 'GUARDAR',
+          buttonCloseText: 'CANCELAR',
           keys: [
             {
-                name: '',
-                label: ''
+                name: 'codeGroup',
+                label: 'Codigo del Grupo'
             },
             {
-                name: '',
-                label: ''
+                name: 'nameGroup',
+                label: 'Nombre del Grupo'
             },
             {
-                name: '',
-                label: ''
+                name: 'descriptionGroup',
+                label: 'Definicion del Grupo'
             },
-          ]
+          ],
+          buttonOpen: () => cerrrarModalAbrirAlerta(),
+          buttonClose: () => handleClose(),
+          success: 'Grupo editado exitosamente'      
         },
         addClass: {
           title: 'Agregar Clase',
-          description: 'xd5',
+          buttonOpenText: 'GUARDAR',
+          buttonCloseText: 'CANCELAR',
+          description: 'Por favor diligencia la siguiente información completando los campos obligatorios(*).',
           keys: [
             {
-                name: '',
-                label: ''
+                name: 'codeNewClass',
+                label: 'Codigo de la Nueva clase'
             },
             {
-                name: '',
-                label: ''
+                name: 'nameNewClass',
+                label: 'Nombre de la Nueva clase'
             },
             {
-                name: '',
-                label: ''
+                name: 'definitionNewClass',
+                label: 'Definicion de la Nueva clase'
             },
-          ]
+          ],
+          buttonOpen: () => cerrrarModalAbrirAlerta(),
+          buttonClose: () => handleClose(),
+          success: 'Clase creada exitosamente'      
         },
         editClass: {
           title: 'Editar Clase',
-          description: 'xd6',
+          buttonOpenText: 'GUARDAR',
+          buttonCloseText: 'CANCELAR',
           keys: [
             {
-                name: '',
-                label: ''
+                name: 'codeClass',
+                label: 'Codigo del Grupo'
             },
             {
-                name: '',
-                label: ''
+                name: 'nameClass',
+                label: 'Nombre del Grupo'
             },
             {
-                name: '',
-                label: ''
+                name: 'descriptionClass',
+                label: 'Definicion del Grupo'
             },
-          ]
+          ],
+          buttonOpen: () => cerrrarModalAbrirAlerta(),
+          buttonClose: () => handleClose(),
+          success: 'Clase editada exitosamente' 
         },
         definition: {
           title: 'Definicion',
+          buttonOpenText: 'ACEPTAR',
           description: <DefinitionText />,
+          buttonAccept: () => handleClose()   
+        },
+        delete: {
+          buttonOpenText: 'ACEPTAR',
+          buttonCloseText: 'CANCELAR',
+          title: '¿Eliminar concepto?',
+          description: '¿Está seguro que desea eliminar este concepto?',
+          desciptionAlert: 'Si el concepto tiene conceptos relacionados en niveles inferiores o transacciones de ingresos, egresos o solicitudes de pago activas, no puede ser eliminado.',
+          success: 'Concepto eliminado exitosamente',
+          error: 'Este concepto no se puede eliminar',
+          buttonOpen: () => cerrrarModalAbrirAlerta(),
+          buttonClose: () => handleClose(),
         }
     }
     return (
       <Paper style={{padding: 12}}>
         <Grid container>
-          <Button variant="outlined" color="primary" onClick={() => handleOpenModela(valuesPerModals.addSection)}>
-                  Agregar Seccion
-          </Button>
-          <Button variant="outlined" color="primary" onClick={() => handleOpenModela(valuesPerModals.editSection)}>
-                  Editar Seccion
-          </Button>
-          <Button variant="outlined" color="primary" onClick={() => handleOpenModela(valuesPerModals.editDivision)}>
-                  Editar Division
-          </Button>
-          <Button variant="outlined" color="primary" onClick={() => handleOpenModela(valuesPerModals.editGroup)}>
-                  Editar Grupo 
-          </Button>
-          <Button variant="outlined" color="primary" onClick={() => handleOpenModela(valuesPerModals.editClass)}>
-                  Editar Clase
-          </Button>
-          <Button variant="outlined" color="primary" onClick={() => handleOpenModela(valuesPerModals.addClass)}>
-                  Agregar Clase 
-          </Button>
-          <Button variant="outlined" color="primary" onClick={() => handleOpenModela(valuesPerModals.definition)}>
+        <AcceptButton variant="outlined" color="primary" onClick={() => handleOpenModela(valuesPerModals.definition)}>
                 Definicion 
-          </Button>
+          </AcceptButton>
+          <AcceptButton variant="outlined" color="primary" onClick={() => handleOpenModela(valuesPerModals.addSection)}>
+                  Agregar Seccion
+          </AcceptButton>
+          <AcceptButton variant="outlined" color="primary" onClick={() => handleOpenModela(valuesPerModals.addClass)}>
+                  Agregar Clase 
+          </AcceptButton>
+          <AcceptButton variant="outlined" color="primary" onClick={() => handleOpenModela(valuesPerModals.editSection)}>
+                  Editar Seccion
+          </AcceptButton>
+          <AcceptButton variant="outlined" color="primary" onClick={() => handleOpenModela(valuesPerModals.editDivision)}>
+                  Editar Division
+          </AcceptButton>
+          <AcceptButton variant="outlined" color="primary" onClick={() => handleOpenModela(valuesPerModals.editGroup)}>
+                  Editar Grupo 
+          </AcceptButton>
+          <AcceptButton variant="outlined" color="primary" onClick={() => handleOpenModela(valuesPerModals.editClass)}>
+                  Editar Clase
+          </AcceptButton>
+          <AcceptButton variant="outlined" color="primary" onClick={() => handleOpenModela(valuesPerModals.delete)}>
+                  Eliminar 
+          </AcceptButton>
         </Grid>
 
-        <Dialog onClose={handleClose} aria-labelledby="customized-dialog-title" open={open}>
+        <EditDialog fullWidth onClose={handleClose} aria-labelledby="customized-dialog-title" open={open}>
           <DialogTitle id="customized-dialog-title" onClose={handleClose}>
-            {info.title} 
-            {' '}
-            {/* {info.icon} */}
+            <span>{info.title} {' '}</span>
+            {info.saveIcon}
           </DialogTitle>
           <DialogContent dividers>
             {info.description}
             <Divider light />
-            <TextfielDescription  keys={info.keys} setInfo={setInfo} info={info} />
+            <GridField container alignItems="center" direction={"column"} spacing={5}>
+            <Grid item >
+              <TextfielDescription  keys={info.keys} setInfo={setInfo} info={info} />
+            </Grid>
+            </GridField>
           </DialogContent>
-          <DialogActions>
-          <Button autoFocus onClick={info.button} color="primary">
-              Save changes
-          </Button>
-          </DialogActions>
-        </Dialog>
+          <ActionsDialog>
+          <CancelButton color="primary" variant="outlined" color="default" onClick={info.buttonClose}>
+              {info.buttonCloseText}
+          </CancelButton>
+          <AcceptButton autoFocus onClick={info.buttonOpen || info.buttonAccept} color="success" variant="outlined" >
+              {info.buttonOpenText}
+          </AcceptButton>
+          </ActionsDialog>
+        </EditDialog>
         <Dialog onClose={() => handleCloseAlert(false)} aria-labelledby="customized-dialog-title" open={openAlert}>
         <DialogTitle id="customized-dialog-title" onClose={() => handleCloseAlert(false)}>
-          Edotado exitosamente
+          {info.success || info.error}
         </DialogTitle>
+        <DialogContent>
+        {info.desciptionAlert}
+        </DialogContent>
         <DialogActions>
-          <Button autoFocus onClick={() => handleCloseAlert(false)} color="primary">
-            Finalizar
-          </Button>
+          <AcceptButton autoFocus onClick={() => handleCloseAlert(false)}  variant="outlined" color="success">
+            FINALIZAR
+          </AcceptButton>
         </DialogActions>
       </Dialog>
       </Paper>
